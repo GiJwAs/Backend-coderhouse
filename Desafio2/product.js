9d00954b2f5313b52ba1db1d21aab0e0b5d2efb1
Daniel Di Leva <danieldileva@hotmail.com>
2022-05-05 19:19:00 -0300

    desafio 2

--- Desafio2/product.js
+++ Desafio2/product.js
@@ -3,21 +3,21 @@ const Contenedor = require('./desafio.js')
 const run = async function () {
     let contenedor = new Contenedor('productos.txt')
     contenedor.save({
-        title: "Pulsera",
-        price: 150,
-        thumbnail: "shorturl.at/bqDZ3"
+        title: "Remera",
+        price: 3450,
+        thumbnail: "https://www.camaronbrujo.com/media/catalog/product…/a/palmera---capuccino-3469-y-verde-ax-5402-h.jpg"
     })
 
     contenedor.save({
-        title: "Collar",
-        price: 100,
-        thumbnail: "shorturl.at/hlEYZ"
+        title: "Pantalon",
+        price: 10000,
+        thumbnail: "https://www.camaronbrujo.com/media/catalog/product…d08d6e5fb8d27136e95/c/a/camaron_original_175_.jpg"
     })
 
     contenedor.save({
-        title: "iPhone",
-        price: 1200,
-        thumbnail: "shorturl.at/dpqH6"
+        title: "Canguro",
+        price: 9500,
+        thumbnail: "	https://www.camaronbrujo.com/media/catalog/product…00/9df78eab33525d08d6e5fb8d27136e95/w/e/web-9.jpg"
     })
     console.log(contenedor.getById(1));
     console.log(contenedor.getById(5));
